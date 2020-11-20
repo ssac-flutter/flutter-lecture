7e0fe8e075b367c9ae90f46f1eb6be19d6937e2a
Junsuk Oh <a811219@gmail.com>
2020-11-20 15:16:27 +0900

    Stream 심화

--- lib/ui/stream_page.dart
+++ lib/ui/stream_page.dart
@@ -4,22 +4,40 @@ import 'package:flutter_app/stream/counter.dart';
 class StreamPage extends StatelessWidget {
   final _counter = Counter();
 
+  final upCounter = Stream<int>
+      .periodic(Duration(seconds: 1), (value) => value)
+      .take(11);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Stream'),
       ),
-      body: StreamBuilder<int>(
-          stream: _counter.count,
-          builder: (context, snapshot) {
-            return Center(
-              child: Text(
-                '${snapshot.data}',
-                style: TextStyle(fontSize: 40),
-              ),
-            );
-          }),
+      body: Column(
+        children: [
+          StreamBuilder<int>(
+              stream: _counter.count,
+              builder: (context, snapshot) {
+                return Center(
+                  child: Text(
+                    '${snapshot.data}',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                );
+              }),
+          StreamBuilder<int>(
+              stream: upCounter,
+              builder: (context, snapshot) {
+                return Center(
+                  child: Text(
+                    '${snapshot.data}',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                );
+              }),
+        ],
+      ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
@@ -31,6 +49,11 @@ class StreamPage extends StatelessWidget {
           FloatingActionButton(
             heroTag: 'fab2',
             child: Icon(Icons.exposure_minus_1),
+            onPressed: _counter.decrease,
+          ),
+          FloatingActionButton(
+            heroTag: 'fab3',
+            child: Icon(Icons.watch_later_outlined),
             onPressed: _counter.decrease,
           ),
         ],
